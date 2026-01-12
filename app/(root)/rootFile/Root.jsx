9cd7b8f7a0c7e2218f18eabdd4c29a4ab2ffe989
NayokCoder <ferdous.anon@gmail.com>
2026-01-13 00:49:39 +0600

    C:/Program Files/Git/

--- app/(root)/rootFile/Root.jsx
+++ app/(root)/rootFile/Root.jsx
@@ -103,16 +103,18 @@ const Root = () => {
       <div className="hidden xl:flex h-full w-full gap-3 lg:gap-5 flex-col lg:flex-row-reverse rounded-xl lg:rounded-2xl p-4 lg:p-0">
         <MenuNav nav={navItems} scrollContainerRef={parentRef} className="hidden lg:flex" />
 
-        <div className="grow h-full overflow-hidden">
-          <ScrollArea className="h-full lg:pe-5 lg:-me-5" viewportRef={parentRef}>
-            <div className="space-y-6 lg:space-y-8 py-4 lg:py-6">
-              {navItems.map((item) => (
-                <div key={item.id} id={item.id} className="space-y-2.5">
-                  <div className="bg-transparent rounded-lg p-4 lg:p-0 shadow-sm">{item.component}</div>
-                </div>
-              ))}
-            </div>
-          </ScrollArea>
+        <div className="grow h-full overflow-hidden 0">
+          <content className=" ">
+            <ScrollArea className="h-full lg:pe-5 lg:-me-5  max-w-5xl mx-auto" viewportRef={parentRef}>
+              <div className="space-y-6 lg:space-y-8 py-4 lg:py-6">
+                {navItems.map((item) => (
+                  <div key={item.id} id={item.id} className="space-y-2.5">
+                    <div className="bg-transparent rounded-lg p-4 lg:p-0 shadow-sm">{item.component}</div>
+                  </div>
+                ))}
+              </div>
+            </ScrollArea>
+          </content>
         </div>
       </div>
     </>
